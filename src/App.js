198e99072f6767e2abcd00d4169923a7f618d1bd
David Soards <davids@gocodigo.com>
2019-03-02 20:08:40 -0500

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app-container">
+        <header>
+          <div class="app-identity">
+            <h1>Calendar Generator</h1>
+          </div>
+          <div class="app-inputs">
+            <input
+              type="date"
+              name="start date"
+              value="2019-01-01"
+              id="start"
+            />
+            <input type="input" name="days" id="days" />
+            <input type="button" value="Update" id="update" />
+          </div>
         </header>
+        <main>
+          <table>
+            <thead>
+              <tr>
+                <th>S</th>
+                <th>M</th>
+                <th>T</th>
+                <th>W</th>
+                <th>T</th>
+                <th>F</th>
+                <th>S</th>
+              </tr>
+            </thead>
+            <thead>
+              <tr>
+                <th colspan="7">
+                  <h2>March 2019</h2>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
       </div>
     );
   }
